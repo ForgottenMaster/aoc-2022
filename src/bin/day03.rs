7fc67643e8716c28fa71a38baa3c409ceb36a8c1
Robin Firth <theforgottenmaster@googlemail.com>
2022-12-03 09:59:31 +0000

    Day 3 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,12 +1,25 @@
+use itertools::*;
+
 const INPUT: &str = include_str!("../input/day03.txt");
 
 #[cfg(not(tarpaulin))]
 fn main() {
     println!("Part 1 => {}", part_1(INPUT));
+    println!("Part 2 => {}", part_2(INPUT));
 }
 
 fn part_1(input: &str) -> u32 {
     input.trim().lines().map(process_line).sum()
+}
+
+fn part_2(input: &str) -> u32 {
+    input
+        .trim()
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(process_chunk)
+        .sum()
 }
 
 fn process_line(input: &str) -> u32 {
@@ -16,6 +29,21 @@ fn process_line(input: &str) -> u32 {
     first_half
         .chars()
         .filter(|character| second_half.contains(*character))
+        .map(char_to_priority)
+        .next()
+        .unwrap()
+}
+
+fn process_chunk<'a>(chunk: impl Iterator<Item = &'a str>) -> u32 {
+    let mut chunk = chunk.map(|line| line.trim());
+    let (elf_1, elf_2, elf_3) = (
+        chunk.next().unwrap(),
+        chunk.next().unwrap(),
+        chunk.next().unwrap(),
+    );
+    elf_1
+        .chars()
+        .filter(|character| elf_2.contains(*character) && elf_3.contains(*character))
         .map(char_to_priority)
         .next()
         .unwrap()
@@ -74,6 +102,18 @@ mod tests {
 
         // Act
         let output = part_1(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // Arrange
+        const EXPECTED: u32 = 70;
+
+        // Act
+        let output = part_2(INPUT);
 
         // Assert
         assert_eq!(output, EXPECTED);
