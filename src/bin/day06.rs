fe5fc4219f64a7087a6fd9013bef538e14453733
Robin Firth <theforgottenmaster@googlemail.com>
2022-12-06 08:17:47 +0000

    Day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,21 +1,27 @@
-use itertools::*;
-
 const INPUT: &str = include_str!("../input/day06.txt");
 
 #[cfg(not(tarpaulin))]
 fn main() {
     println!("Part 1 => {}", part_1(INPUT));
+    println!("Part 2 => {}", part_2(INPUT));
 }
 
 fn part_1(input: &str) -> usize {
-    input
-        .trim()
-        .chars()
-        .tuple_windows()
+    process_input(input, 4)
+}
+
+fn part_2(input: &str) -> usize {
+    process_input(input, 14)
+}
+
+fn process_input(input: &str, window_size: usize) -> usize {
+    let bytes = input.trim().as_bytes();
+    bytes
+        .windows(window_size)
         .enumerate()
-        .filter_map(|(index, tuple)| {
-            if chars_in_tuple_are_unique(tuple) {
-                Some(index + 4)
+        .filter_map(|(index, window)| {
+            if bytes_in_slice_are_unique(window) {
+                Some(index + window_size)
             } else {
                 None
             }
@@ -24,14 +30,14 @@ fn part_1(input: &str) -> usize {
         .unwrap()
 }
 
-fn chars_in_tuple_are_unique(tuple: (char, char, char, char)) -> bool {
-    let (first, second, third, fourth) = tuple;
-    first != second
-        && first != third
-        && first != fourth
-        && second != third
-        && second != fourth
-        && third != fourth
+fn bytes_in_slice_are_unique(slice: &[u8]) -> bool {
+    (0..slice.len() - 1).all(|first_index| {
+        let first_byte = slice[first_index];
+        !(first_index + 1..slice.len()).any(|second_index| {
+            let second_byte = slice[second_index];
+            first_byte == second_byte
+        })
+    })
 }
 
 #[cfg(test)]
@@ -72,6 +78,45 @@ mod tests {
 
         // Act
         let output = part_1(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2_1() {
+        // Arrange
+        const INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        const EXPECTED: usize = 19;
+
+        // Act
+        let output = part_2(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2_2() {
+        // Arrange
+        const INPUT: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        const EXPECTED: usize = 23;
+
+        // Act
+        let output = part_2(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_3_3() {
+        // Arrange
+        const INPUT: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+        const EXPECTED: usize = 23;
+
+        // Act
+        let output = part_2(INPUT);
 
         // Assert
         assert_eq!(output, EXPECTED);
