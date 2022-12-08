f76182788a49cdcc0982d843c21722eaba1651f2
Robin Firth <theforgottenmaster@googlemail.com>
2022-12-08 23:13:33 +0000

    Day 8 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -5,6 +5,7 @@ const INPUT: &str = include_str!("../input/day08.txt");
 #[cfg(not(tarpaulin))]
 fn main() {
     println!("Part 1 => {}", part_1(INPUT));
+    println!("Part 2 => {}", part_2(INPUT));
 }
 
 mod forest {
@@ -23,6 +24,21 @@ mod forest {
         pub fn iter_visible(&self) -> impl Iterator<Item = ((usize, usize), u8)> + '_ {
             self.iter().filter(|(coord, elem_1)| {
                 self.is_tree_visible(*coord, |(_, elem_2)| elem_2 >= *elem_1)
+            })
+        }
+
+        pub fn iter_scenic_scores(&self) -> impl Iterator<Item = u32> + '_ {
+            self.iter().map(|(coord, elem_1)| {
+                self.scenic_score(coord, |encountered_block, (_, elem_2)| {
+                    if *encountered_block {
+                        None
+                    } else if elem_2 < elem_1 {
+                        Some(())
+                    } else {
+                        *encountered_block = true;
+                        Some(())
+                    }
+                })
             })
         }
 
@@ -123,6 +139,18 @@ mod forest {
                 || !self.iter_south(coord).any(&mut func)
                 || !self.iter_west(coord).any(&mut func)
         }
+
+        fn scenic_score(
+            &self,
+            coord: (usize, usize),
+            mut func: impl FnMut(&mut bool, ((usize, usize), u8)) -> Option<()>,
+        ) -> u32 {
+            let score_north = self.iter_north(coord).scan(false, &mut func).count() as u32;
+            let score_west = self.iter_west(coord).scan(false, &mut func).count() as u32;
+            let score_south = self.iter_south(coord).scan(false, &mut func).count() as u32;
+            let score_east = self.iter_east(coord).scan(false, &mut func).count() as u32;
+            score_north * score_west * score_south * score_east
+        }
     }
 
     impl FromStr for Forest {
@@ -161,6 +189,15 @@ fn part_1(input: &str) -> u32 {
     input.parse::<Forest>().unwrap().iter_visible().count() as u32
 }
 
+fn part_2(input: &str) -> u32 {
+    input
+        .parse::<Forest>()
+        .unwrap()
+        .iter_scenic_scores()
+        .max()
+        .unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -180,6 +217,18 @@ mod tests {
 
         // Act
         let output = part_1(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // Arrange
+        const EXPECTED: u32 = 8;
+
+        // Act
+        let output = part_2(INPUT);
 
         // Assert
         assert_eq!(output, EXPECTED);
