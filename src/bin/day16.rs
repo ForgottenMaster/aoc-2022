5e4e206ae7b360ba055780857d98bdb424403e00
Robin Firth <theforgottenmaster@googlemail.com>
2022-12-17 18:28:41 +0000

    Day 16 part 1 rev 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -10,6 +10,7 @@ type GraphType<'a> = Graph<(&'a str, u32), u32>;
 #[cfg(not(tarpaulin))]
 fn main() {
     println!("Part 1 => {}", part_1(INPUT));
+    println!("Part 2 => {}", part_2(INPUT));
 }
 
 fn part_1(input: &str) -> u32 {
@@ -19,6 +20,10 @@ fn part_1(input: &str) -> u32 {
     opened.insert(starting_node);
     let mut cache = BTreeMap::new();
     calculate_maximum_total_flow(&graph, opened, &mut cache, 30, starting_node)
+}
+
+fn part_2(_input: &str) -> u32 {
+    todo!()
 }
 
 fn calculate_maximum_total_flow(
@@ -172,6 +177,30 @@ mod tests {
 
         // Act
         let output = part_1(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // Arrange
+        const INPUT: &str = "
+        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II
+        ";
+        const EXPECTED: u32 = 1707;
+
+        // Act
+        let output = part_2(INPUT);
 
         // Assert
         assert_eq!(output, EXPECTED);
