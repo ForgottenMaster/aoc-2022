14dbee12962dbc996b8ce10bde28f0ab9e2cfcb2
Robin Firth <theforgottenmaster@googlemail.com>
2022-12-23 00:57:08 +0000

    day 20 part 2

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,26 +1,41 @@
 const INPUT: &str = include_str!("../input/day20.txt");
 
+type NumberType = i64;
+
 #[cfg(not(tarpaulin))]
 fn main() {
     println!("Part 1 => {}", part_1(INPUT));
+    println!("Part 2 => {}", part_2(INPUT));
 }
 
-fn part_1(input: &str) -> i32 {
+fn part_1(input: &str) -> NumberType {
     let numbers = parse_input(input);
     let numbers = mix(numbers);
     extract_answer(numbers)
 }
 
-fn parse_input(input: &str) -> Vec<i32> {
+fn part_2(input: &str) -> NumberType {
+    let numbers = parse_input(input);
+    let mut numbers = numbers
+        .into_iter()
+        .map(|(index, value)| (index, value * 811589153))
+        .collect();
+    for _ in 0..10 {
+        numbers = mix(numbers);
+    }
+    extract_answer(numbers)
+}
+
+fn parse_input(input: &str) -> Vec<(usize, NumberType)> {
     input
         .trim()
         .lines()
-        .map(|line| line.trim().parse::<i32>().unwrap())
+        .map(|line| line.trim().parse().unwrap())
+        .enumerate()
         .collect()
 }
 
-fn mix(numbers: Vec<i32>) -> Vec<i32> {
-    let mut numbers = numbers.into_iter().enumerate().collect::<Vec<_>>();
+fn mix(mut numbers: Vec<(usize, NumberType)>) -> Vec<(usize, NumberType)> {
     (0..numbers.len()).for_each(|order_to_find| {
         let (index, number, order) = numbers
             .iter()
@@ -37,22 +52,20 @@ fn mix(numbers: Vec<i32>) -> Vec<i32> {
 
         if number != 0 {
             numbers.retain(|(other_order, _)| *other_order != order);
-            let mut index = index as i32 + number;
-            while index < 0 {
-                index += numbers.len() as i32;
-            }
-            while index >= numbers.len() as i32 {
-                index -= numbers.len() as i32;
+            let mut index = (index as NumberType + number) % numbers.len() as NumberType;
+            if index < 0 {
+                index += numbers.len() as NumberType;
             }
             numbers.insert(index as usize, (order, number));
         }
     });
-    numbers.into_iter().map(|(_, number)| number).collect()
+    numbers
 }
 
-fn extract_answer(numbers: Vec<i32>) -> i32 {
+fn extract_answer(numbers: Vec<(usize, NumberType)>) -> NumberType {
     numbers
         .into_iter()
+        .map(|(_, number)| number)
         .cycle()
         .skip_while(|number| *number != 0)
         .skip(1000)
@@ -77,10 +90,31 @@ mod tests {
         0
         4
         ";
-        const EXPECTED: i32 = 3;
+        const EXPECTED: NumberType = 3;
 
         // Act
         let output = part_1(INPUT);
+
+        // Assert
+        assert_eq!(output, EXPECTED);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // Arrange
+        const INPUT: &str = "
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
+        ";
+        const EXPECTED: NumberType = 1623178306;
+
+        // Act
+        let output = part_2(INPUT);
 
         // Assert
         assert_eq!(output, EXPECTED);
